6f79b92bdcf470b10879f34584c113a40ca1d1ea
ChrisCatto <chriscatto08@gmail.com>
2023-10-27 21:38:48 +0300

    xaga: Use `libhidlbase-v32` for MTK Thermal HAL
    linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.thermal@2.0-service.mtk": cannot locate symbol "_ZN7android8String16aSEOS0_" referenced by "/apex/com.android.vndk.v34/lib64/libhidlbase.so"
    
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -461,6 +461,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libutils-v32
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libhidlbase-v32.so
+    
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service-lazy
